7101a056eb140bcf1e523428850211a6084aea88
employee <suland@greenriver.edu>
2021-10-13 07:52:14 -0700

    Initial commit

---
+++ src/writingtools/Pencil.java
@@ -0,0 +1,11 @@
+package writingtools;
+
+/**
+ * Write a pencil class that stores the manufacturer(STAEDTLER, Paper Mate, etc),
+ * point size (0.2 mm, 0.5 mm, etc)
+ * and type, (mechanical or wooden)
+ * This class should be comparable and sort pencils based on point size.
+ */
+public class Pencil {
+
+}
---
+++ tests/writingtools/PencilTest.java
@@ -0,0 +1,13 @@
+package writingtools;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * This class should test your methods in your Pencil class
+ */
+public class PencilTest {
+
+
+
+}
